2a0c5c9519fb6ce87f9b7d7da22370ba33a82291
Aaronverdoold <146937567+Aaronverdoold@users.noreply.github.com>
2023-11-29 12:39:46 +0100

    Update inlog.php

---
+++ website ding/inlog.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha384-dfZpZjJZKLOpXDQm2aDM0jF7efvH+2T8XzUmikXr6q0jZ9ZlHAAKRII5oD33J4k5" crossorigin="anonymous">
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <img src="https://art.pixilart.com/fb41e74302b2297.png" alt="Logo">
+        </div>
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+        </div>
+        
+</div>
+    </nav>
+
+
+    <div class="iets">
+        <div class="div-links">
+            <div class="sidebar">
+                <a href="#"><p>Home</p></a>
+                <a href="Forum.html"><p>Forums</p></a>
+                <a href="NAV-HTML.html"><p>Games</p></a>
+            </div>
+        </div>
+
+    
