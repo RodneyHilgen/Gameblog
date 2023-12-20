e30f5e315a23310fea13af5789a38cbf58cf8b88
Rodney Hilgen <rodneyhilgen@live.nl>
2023-12-20 12:27:40 +0100

    z

--- js/script.js
+++ js/script.js
@@ -1,113 +1,119 @@
 let currentIndex = 0;
-const slides = document.querySelector('.slides');
-const balls = document.querySelectorAll('.ball');
+const slides = document.querySelector(".slides");
+const balls = document.querySelectorAll(".ball");
 let slideInterval;
 
 function changeSlide(index) {
-    currentIndex = index;
-    updateSlider();
-    resetTimer();
+  currentIndex = index;
+  updateSlider();
+  resetTimer();
 }
 
 function updateSlider() {
-    slides.style.transform = `translateX(${-currentIndex * 100}%)`;
+  slides.style.transform = `translateX(${-currentIndex * 100}%)`;
 
-    balls.forEach((ball, index) => {
-        if (index === currentIndex) {
-            ball.style.backgroundColor = '#555';
-        } else {
-            ball.style.backgroundColor = '#333';
-        }
-    });
+  balls.forEach((ball, index) => {
+    if (index === currentIndex) {
+      ball.style.backgroundColor = "#555";
+    } else {
+      ball.style.backgroundColor = "#333";
+    }
+  });
 }
 
 function resetTimer() {
-    clearInterval(slideInterval);
-    slideInterval = setInterval(() => {
-        currentIndex = (currentIndex + 1) % balls.length;
-        updateSlider();
-    }, 3000);
+  clearInterval(slideInterval);
+  slideInterval = setInterval(() => {
+    currentIndex = (currentIndex + 1) % balls.length;
+    updateSlider();
+  }, 3000);
 }
 
 slideInterval = setInterval(() => {
-    currentIndex = (currentIndex + 1) % balls.length;
-    updateSlider();
+  currentIndex = (currentIndex + 1) % balls.length;
+  updateSlider();
 }, 3000);
 
 //calender//
 
-document.addEventListener('DOMContentLoaded', function() {
-    const daysContainer = document.getElementById('days');
-    const currentMonthElement = document.getElementById('currentMonth');
-    let currentMonth = 10; // November (0-indexed)
+document.addEventListener("DOMContentLoaded", function () {
+  const daysContainer = document.getElementById("days");
+  const currentMonthElement = document.getElementById("currentMonth");
+  let currentMonth = 10; // November (0-indexed)
 
-    function updateCalendar() {
-        daysContainer.innerHTML = '';
+  function updateCalendar() {
+    daysContainer.innerHTML = "";
 
-        const daysInMonth = new Date(2023, currentMonth + 1, 0).getDate();
+    const daysInMonth = new Date(2023, currentMonth + 1, 0).getDate();
 
-        for (let i = 1; i <= daysInMonth; i++) {
-            const dayElement = document.createElement('div');
-            dayElement.textContent = i;
-            daysContainer.appendChild(dayElement);
-        }
-
-        currentMonthElement.textContent = new Date(2023, currentMonth, 1).toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
+    for (let i = 1; i <= daysInMonth; i++) {
+      const dayElement = document.createElement("div");
+      dayElement.textContent = i;
+      daysContainer.appendChild(dayElement);
     }
 
-    document.getElementById('prevMonth').addEventListener('click', function() {
-        currentMonth--;
-        updateCalendar();
-    });
+    currentMonthElement.textContent = new Date(
+      2023,
+      currentMonth,
+      1
+    ).toLocaleDateString("en-US", { month: "long", year: "numeric" });
+  }
 
-    document.getElementById('nextMonth').addEventListener('click', function() {
-        currentMonth++;
-        updateCalendar();
-    });
-
+  document.getElementById("prevMonth").addEventListener("click", function () {
+    currentMonth--;
     updateCalendar();
+  });
+
+  document.getElementById("nextMonth").addEventListener("click", function () {
+    currentMonth++;
+    updateCalendar();
+  });
+
+  updateCalendar();
 });
 //poll
-const optionsContainer = document.getElementById('options');
-const resultsContainer = document.getElementById('results');
+const optionsContainer = document.getElementById("options");
+const resultsContainer = document.getElementById("results");
 
 const votes = {};
-let resultsTitle = 'Poll Results';
+let resultsTitle = "Poll Results";
 
-optionsContainer.addEventListener('click', function(event) {
-    const selectedOption = event.target.dataset.option;
+optionsContainer.addEventListener("click", function (event) {
+  const selectedOption = event.target.dataset.option;
 
-    if (selectedOption) {
-        votes[selectedOption] = (votes[selectedOption] || 0) + 1;
-        updateResults();
-    }
+  if (selectedOption) {
+    votes[selectedOption] = (votes[selectedOption] || 0) + 1;
+    updateResults();
+  }
 });
 
 function updateResults() {
-    resultsContainer.innerHTML = `<h3>${resultsTitle}:</h3>`;
+  resultsContainer.innerHTML = `<h3>${resultsTitle}:</h3>`;
 
-    const optionElements = optionsContainer.querySelectorAll('[data-option]');
-    
-    optionElements.forEach(optionElement => {
-        const option = optionElement.dataset.option;
-        const optionText = optionElement.textContent;
-        const voteCount = votes[option] || 0;
+  const optionElements = optionsContainer.querySelectorAll("[data-option]");
 
-        const resultElement = document.createElement('div');
-        resultElement.textContent = `${optionText}: ${voteCount}`;
-        resultsContainer.appendChild(resultElement);
-    });
+  optionElements.forEach((optionElement) => {
+    const option = optionElement.dataset.option;
+    const optionText = optionElement.textContent;
+    const voteCount = votes[option] || 0;
+
+    const resultElement = document.createElement("div");
+    resultElement.textContent = `${optionText}: ${voteCount}`;
+    resultsContainer.appendChild(resultElement);
+  });
 }
- // Code voor de hamburger menu
+// Code voor de hamburger menu
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const sidebar = document.querySelector(".sidebar");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("active");
+  sidebar.classList.toggle("active");
+});
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".sidebar").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+    sidebar.classList.remove("active");
+  })
+);
