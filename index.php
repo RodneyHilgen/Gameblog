f4e913208c7ffe06e0ab48bf873ba59c378b12fd
Aaronverdoold <146937567+Aaronverdoold@users.noreply.github.com>
2024-01-10 13:27:47 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/Inlog.css" />
+</head>
+<body>
+    <nav>
+    <div class="logo">
+        <img src="https://art.pixilart.com/fb41e74302b2297.png" alt="Logo" />
+    </div>
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+        </div>
+        
+    </nav>
+
+
+    <div class="iets">
+        <div class="div-links">
+            <div class="sidebar">
+                <a href="#"><p>Home</p></a>
+                <a href="Forum.html"><p>Forums</p></a>
+                <a href="NAV-HTML.html"><p>Games</p></a>
+            </div>
+        </div>
+
+
+        <div class="div-mid">
+        <h1 class="inlog">Inloggen</h1>
+        <form action="login2.php" method="post" class="inlog1">
+            <label for="email">E-mail:</label>
+            <input type="text" id="email" name="email"placeholder="Email" size="25"><br><br>      
+            <label for="password">Password:</label>
+            <input type="text" id="password" name="password"placeholder="Password"><br><br>
+            <h2>Druk op de button om in te loggen.</h2>
+            <input type="submit" value="Inloggen" class="inlog-button">
+        </form>  
+        </div>
+        <div class="div-rechts"></div>
+            <div class="sidebar-right"></div>  
+        </div>
+    </div>
+
+    
+</body>
+
+    <footer>
+      <div class="social-links">
+        <a href="https://www.instagram.com/" target="_blank"
+          ><img src="img/insta trans.png" alt="Instagram"
+        /></a>
+        <a href="https://www.facebook.com/" target="_blank"
+          ><img src="img/facebook trans.png" alt="Facebook"
+        /></a>
+        <a href="https://twitter.com/" target="_blank"
+          ><img src="img/twitter trans.png" alt="Twitter"
+        /></a>
+      </div>
+    </footer>
+    
+ 
+</html>
+
+
